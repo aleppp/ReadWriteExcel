62b641de6a1c63b1fff751d129586b164218c65f
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2023-04-12 16:18:07 +0800

    Add project files.

---
+++ ReadWriteExcel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Open the text file using a stream reader.
+            using (StreamReader sr = new StreamReader("example.txt"))
+            {
+                // Read the entire file and store its contents in a string.
+                string fileContents = sr.ReadToEnd();
+
+                // Display the file contents to the console.
+                Console.WriteLine(fileContents);
+            }
+        }
+        catch (Exception e)
+        {
+            // If an error occurs, display it to the console.
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
